9a639e7063df032f5de8171db2ee5adf8ee110d1
frmia18 <50294841+frmia18@users.noreply.github.com>
2020-10-21 08:35:10 +0700

    Add files via upload

---
+++ kambing.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Frda
+ */
+public class kambing {
+ public void tambahKambing() {
+        //deklarasi variabel lokal
+        int jumlahKambing = 0;
+
+        jumlahKambing = jumlahKambing +5;
+        System.out.println("Jumlah Kambing setelah ditambah: " + 
+                jumlahKambing;
+    }
+    /**
+     */
+    public void tambahkambing() {
+        //deklarasi variabel lokal
+        int jumlahKambing = 0;
+
+        jumlahKambing = jumlahKambing +5;
+        System.out.println("Jumlah Kambing setelah ditambah: " + 
+                jumlahKambing;
+    }
+    public static void main(String[] args) {
+  kambing kambingJantan = new kambing();
+        kambingJantan.tambahKambing();
+    }
+
+}
